758197ca1693eda13c75efe4dbded3345c6c426e
Bilal-19 <bilalmuhammadyousuf543@gmail.com>
2024-12-07 17:58:32 +0500

    'About' page added.

--- resources/views/User/About.blade.php
+++ resources/views/User/About.blade.php
@@ -2,8 +2,82 @@
 
 
 @section('user-main-section')
+    <div class="row d-flex justify-content-around mt-3">
+        <div class="col-md-5">
+            <p class="text-uppercase fw-bold">Brand Story</p>
+            <p>Sterling Executive Residential is a high quality serviced apartment operator in London, delivering a fresh
+                serviced apartment stay experiences in the city. Whether it's practical corporate housing or
+                relocation, executive class business traveller, or a small group for a business project, Citadel
+                Apartments is the right solution. Whether it's short or medium term stay, customers will appreciate
+                the additional space, privacy, comfort and freedom of our serviced apartments at prices they'll
+                appreciate.
+                <br><br>
+                Sterling Executive Residential is a safe way to rent a private apartment with the flexibility to avoid a
+                tenancy
+                agreement and only stay for the desired length.
+                <br><br>
+                Sterling Executive Residential is relatively new to the corporate apartment sector having only begun its
+                operations in 2015. The brand is rapidly expanding with a compound annual growth rate (CAGR) of
+                145% forecasted for the next year.
+                Sterling Executive Residential rents its own properties as opposed to many
+                other online travel agencies that list apartments from third parties on their sites. Most of our
+                properties are in the three to four-star category with luxury properties coming soon. The apartments
+                are available with one or two bedrooms across Camden, Hackney, Islington, City of London, and
+                Tower Hamlets.
+            </p>
+        </div>
+        <div class="col-md-4">
+            <p class="text-uppercase fw-bold">Our Mission</p>
+            <p>We exist to provide our customers with a wide range of high-quality serviced apartments across the
+                capital London.
+            </p>
 
-<div class="row">
-<h3>About</h3>
-</div>
+
+            <p class="text-uppercase fw-bold">Our Vision</p>
+            <p>
+                Our long-term vision is to revolutionise the service apartments sector to make it accessible and
+                affordable to both residents as well as business travellers.
+                <br>
+                Our short-term vision (3-5 years) is to become the No.1 choice in the corporate serviced apartments
+                sector.
+            </p>
+
+
+            <p class="text-uppercase fw-bold">Our Values</p>
+            <p>
+                Customer-focus: Customers always come first.
+                <br>
+                People: We invest in our greatest asset.
+                <br>
+                Passion: We love what we do!
+                <br>
+                Excellence: We push ourselves to constantly look for ways to improve our operations.
+                <br>
+                Honesty: We mean what we say, we say what we mean.
+            </p>
+        </div>
+    </div>
+
+    <div class="row">
+        <div class="col-md-6 mx-auto">
+            <p class="fw-bold text-center">
+                "Customer service is at the heart of everything we do, as a company and as individuals. In fact, it is
+                the most important aspect of our operations. It is truly our promise to deliver."
+            </p>
+        </div>
+    </div>
+
+    <div class="row mt-2">
+        <div class="col-md-6 mx-auto">
+            <p>
+                Citadel Apartments is a subsidiary of Al Sana Group (please link to ASG website), a private equity
+                and investment firm with interests in Real Estate, Hospitality & Tourism, and Finance
+            </p>
+        </div>
+    </div>
+
+    {{-- Neighborhood Section --}}
+
+
+    {{-- Blog Section --}}
 @endsection
