d1badf841881fbf73e7e1d81bf26db5211609a82
Bilal-19 <bilalmuhammadyousuf543@gmail.com>
2025-01-30 13:11:23 +0500

    Updated footer.

--- resources/views/UserLayout/footer.blade.php
+++ resources/views/UserLayout/footer.blade.php
@@ -1,44 +1,45 @@
 @stack('footer-cta')
 <div class="container-fluid text-light pb-2 " id="footer-color">
     <div class="row d-flex flex-md-row flex-sm-column justify-content-center align-items-around pt-5">
-        <div class="col-md-4">
-            <h5 class="ff-poppins">
-                <a href="{{route('Landing.Page')}}" class="text-light" id="footer-link">
-                    <img src="{{ asset('assets/images/company_logo.png') }}" alt="Sterling Executive Residential Logo"
-                        style="height: 60px; width: 60px;">
-                    STERLING EXECUTIVE RESIDENTIAL
+
+        <div class="col-md-2">
+            <p class="">Company</p>
+            <a href="{{ route('View.About') }}" class="text-light d-block ">About Sterling</a>
+            <a href="#" class="text-light d-block ">Locations</a>
+            <a href="#" class="text-light d-block ">FAQs</a>
+            <a href="{{ route('View.Enquiry.Form') }}" class="text-light d-block ">Contact Us</a>
+        </div>
+
+        <div class="col-md-2">
+            <p class="">Book</p>
+            <a href="{{ route('View.Enquiry.Form') }}" class="text-light d-block ">Make an Enquiry</a>
+            <a href="#" class="text-light d-block ">Booking FAQs</a>
+        </div>
+        <div class="col-md-3 d-flex flex-column">
+            <p>Support</p>
+            <a href="mailto:info@sterling-executive.com" class="text-light">info@sterling-executive.com</a>
+            <a href="#" class="text-light ">Support FAQs</a>
+        </div>
+        <div class="col-md-4 d-flex flex-column">
+            <a href="{{route('Landing.Page')}}" class="text-light" id="footer-link">
+                <img src="{{ asset('assets/images/company_logo.png') }}" alt="Sterling Executive Residential Logo"
+                    style="height: 60px; width: 60px;">
+                STERLING EXECUTIVE RESIDENTIAL
+            </a>
+
+            <div class="mt-2">
+                <a href="#">
+                    <i class="fa-brands fa-linkedin fa-xl"></i>
                 </a>
-            </h5>
-        </div>
-        <div class="col-md-2">
-            <p class="ff-inter">Sitemap</p>
-            <a href="{{ route('Landing.Page') }}" class="text-light d-block ff-inter">Home</a>
-            <a href="{{ route('View.About') }}" class="text-light d-block ff-inter">About Us</a>
-            <a href="{{ route('View.Corporate') }}" class="text-light d-block ff-inter">Corporate</a>
-            <a href="{{ route('View.Benefits') }}" class="text-light d-block ff-inter">Benefits</a>
-            <a href="{{ route('View.Enquiry.Form') }}" class="text-light d-block ff-inter">Contact Us</a>
-            <a href="{{ route('View.Blogs') }}" class="text-light d-block ff-inter">Blogs</a>
-        </div>
-        <div class="col-md-3">
-            <p>Contact Us</p>
-            <a href="tel:+44 7921919426" class="text-light d-block">+44 7921919426</a>
-            <a href="mailto:info@sterling-executive.com" class="text-light d-block">info@sterling-executive.com</a>
-        </div>
-        <div class="col-md-2">
-            <p>Follow us</p>
-
-            <a href="#">
-                <i class="fa-brands fa-linkedin fa-xl"></i>
-            </a>
-            <span>|</span>
-            <a href="https://www.facebook.com/profile.php?id=61569866642277">
-                <i class="fa-brands fa-facebook fa-xl"></i>
-            </a>
-            <span>|</span>
-            <a href="https://www.instagram.com/sterlingexecutive/profilecard/?igsh=MXR3NDNicml4enc4cQ==">
-                <i class="fa-brands fa-instagram fa-xl"></i>
-            </a>
-
+                <span>|</span>
+                <a href="https://www.facebook.com/profile.php?id=61569866642277">
+                    <i class="fa-brands fa-facebook fa-xl"></i>
+                </a>
+                <span>|</span>
+                <a href="https://www.instagram.com/sterlingexecutive/profilecard/?igsh=MXR3NDNicml4enc4cQ==">
+                    <i class="fa-brands fa-instagram fa-xl"></i>
+                </a>
+            </div>
         </div>
     </div>
 
