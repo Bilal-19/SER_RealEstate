88ef144378dbc24bb7db0a5a0f4572d1ba170c38
Bilal-19 <bilalmuhammadyousuf543@gmail.com>
2025-01-31 12:07:11 +0500

    Fized customer inquiry admin page bug.

--- resources/views/Admin/CustomerQueries.blade.php
+++ resources/views/Admin/CustomerQueries.blade.php
@@ -2,17 +2,20 @@
 @section('main-section')
     <div class="container-fluid">
         <div class="row mt-3">
-            <h3 class="text-center  fw-bold">Customer Inquiries</h3>
+            <h3 class="text-center mt-3">Customer <span class="fw-bold">Inquiries</span></h3>
         </div>
 
-        <div class="row ff-inter">
+        <div class="row">
             <div class="col-md-12">
                 <table class="table table-bordered table-striped">
                     <tr>
                         <th>ID</th>
-                        <th>Customer Name</th>
-                        <th>Customer Email</th>
-                        <th>Customer Message</th>
+                        <th>Name</th>
+                        <th>Email</th>
+                        <th>Message</th>
+                        <th>Phone Number</th>
+                        <th>Duration of Stay</th>
+                        <th>Inquiry Date</th>
                     </tr>
 
                     @foreach ($fetchQueries as $rec)
@@ -20,7 +23,12 @@
                             <td>{{ $rec->id }}</td>
                             <td>{{ $rec->name }}</td>
                             <td>{{ $rec->email }}</td>
-                            <td>{{ $rec->message }}</td>
+                            <td>{{ $rec->enquiry_message }}</td>
+                            <td>{{ $rec->phone_number }}</td>
+                            <td>{{ $rec->duration_of_stay }}</td>
+                            <td>
+                                {{ date('d M Y', strtotime($rec->created_at)) }}
+                            </td>
                         </tr>
                     @endforeach
                 </table>
