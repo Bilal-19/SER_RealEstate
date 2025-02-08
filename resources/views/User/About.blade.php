fd6d57541a91c1828bdfdecf8c9fa737ae42e3c8
Bilal-19 <bilalmuhammadyousuf543@gmail.com>
2025-02-08 15:19:43 +0500

    Updated about page.

--- resources/views/User/About.blade.php
+++ resources/views/User/About.blade.php
@@ -4,35 +4,43 @@
     <style>
         .banner-img {
             background-image: url('/assets/images/about_banner.jpg');
-            background-size: 100%;
-            background-attachment: fixed;
+            background-size: cover;
+            background-attachment: scroll;
             background-repeat: no-repeat;
+            height:1000px;
+            border-radius: 15px;
+            /* background-color: rgba(0, 0, 0, 0.25); */
+            /* width: 90%; */
         }
     </style>
 @endpush
 
 @push('CTA')
-    <div class="row mt-5">
-        <div class="col-md-9 mx-auto text-light search-container banner-img">
-            <h2 class="text-center  fs-48">
-                About Sterling
-            </h2>
+    <div class="container-fluid banner-img">
+        <div class="row">
+            <div class="col-md-12 text-center text-light search-container">
+                <h2 class="fs-48 fs-sm-25 mt-150">
+                    About Sterling
+                </h2>
+            </div>
         </div>
     </div>
 @endpush
 
 @section('user-main-section')
-    <div class="row mt-5 mb-5">
-        <div class="col-md-12">
-            <p>
-                Welcome to Sterling Executive, your premier destination for unparalleled corporate accommodation solutions.
-                At Sterling Executive, we understand the diverse needs of business travellers, and we are committed to
-                providing a seamless and sophisticated experience. Our platform offers an extensive portfolio of
-                high-quality, professionally-managed properties, tailored to suit the corporate traveller's lifestyle. With
-                a focus on comfort, convenience, and connectivity, Sterling Executive ensures that your stay is as
-                productive and pleasant as possible. Our dedicated support team is always on hand to assist, ensuring that
-                every aspect of your travel is taken care of with precision and expertise.
-            </p>
+    <div class="container-fluid mt-100 mt-sm-25">
+        <div class="row">
+            <div class="col-md-12">
+                <p>
+                    Welcome to Sterling Executive, your premier destination for unparalleled corporate accommodation solutions.
+                    At Sterling Executive, we understand the diverse needs of business travellers, and we are committed to
+                    providing a seamless and sophisticated experience. Our platform offers an extensive portfolio of
+                    high-quality, professionally-managed properties, tailored to suit the corporate traveller's lifestyle. With
+                    a focus on comfort, convenience, and connectivity, Sterling Executive ensures that your stay is as
+                    productive and pleasant as possible. Our dedicated support team is always on hand to assist, ensuring that
+                    every aspect of your travel is taken care of with precision and expertise.
+                </p>
+            </div>
         </div>
     </div>
 
