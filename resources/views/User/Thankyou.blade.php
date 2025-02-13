71e8a1c6f48366fba4025e1b368c2a0dd10f9005
Bilal-19 <bilalmuhammadyousuf543@gmail.com>
2025-02-13 09:45:59 +0500

    Fixed thankyou page layout.

--- resources/views/User/Thankyou.blade.php
+++ resources/views/User/Thankyou.blade.php
@@ -30,20 +30,21 @@
 @endpush
 
 @push('CTA')
-    <div class="row shadow">
+<div class="container-fluid mt-150">
+    <div class="row">
         <div class="col-md-8 mx-auto style-container">
             <img src="{{ asset('assets/images/circle.jpg') }}" alt="confirmation tick" class="img-fluid mt-5">
             <h4 class="fs-40 text-center mt-4">Thank You</h4>
             <p class="fs-18 text-center">We sent you a conformation email and <br> we will contact you shortly </p>
 
-            <div class="d-flex justify-content-around align-items-center mt-5 bg-pale-gray p-16">
+            <div class="d-flex justify-content-around align-items-center mt-5 bg-pale-gray p-3 rounded">
                 <div class="d-flex justify-content-between align-items-center">
                     <div>
                         <img src="{{ asset("assets/images/phone.png") }}" alt="whatsApp Icon" class="img-fluid icon">
                     </div>
                     <div class="mx-2">
-                        <p class="mb-0  fw-semibold">Phone</p>
-                        <p class="mb-0  fs-18">+44 7921919426</p>
+                        <p class="mb-0 fw-semibold">Phone</p>
+                        <p class="mb-0 fs-18">+44 7921919426</p>
                     </div>
                 </div>
 
@@ -52,13 +53,14 @@
                         <img src="{{ asset("assets/images/email.png") }}" alt="Email Icon" class="img-fluid icon">
                     </div>
                     <div class="mx-2">
-                        <p class="mb-0  fw-semibold">Email</p>
-                        <p class="mb-0  fs-18">zain.rav@gmail.com</p>
+                        <p class="mb-0 fw-semibold">Email</p>
+                        <p class="mb-0 fs-18">zain.rav@gmail.com</p>
                     </div>
                 </div>
             </div>
         </div>
     </div>
+</div>
 @endpush
 @section('user-main-section')
 @endsection
