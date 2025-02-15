e1dcf3d300d5453cfc83894ff2b67fa9fe257541
Bilal-19 <bilalmuhammadyousuf543@gmail.com>
2025-02-15 11:15:22 +0500

    Removed JS CDN from 'payment voucher' file,

--- resources/views/Mail/Voucher.blade.php
+++ resources/views/Mail/Voucher.blade.php
@@ -50,10 +50,6 @@
             Thank you for your e-payment. If you have any questions, please contact us at info@sterling-executive.com
         </footer>
     </div>
-
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js"
-        integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous">
-    </script>
 </body>
 
 </html>
